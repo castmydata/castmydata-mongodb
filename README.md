a758a7544d960b39be1fb46df45d839e43b5b8ae
Zulfa Juniadi bin Zulkifli <zulfajuniadi@gmail.com>
2016-07-16 03:44:06 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,73 @@
+# CastMyData Mongodb
+
+MongoDB Backend Plugin for CastMyData
+
+## Installation
+
+npm install --save castmydata-mongodb
+
+## Usage
+
+```javascript
+castmydata.start({
+    db: require('castmydata-mongodb'),
+    mongoURL: 'mongodb://localhost:27017/test', // MongoDB URL
+    mongoConnOptions: {},                       // Optional Connection Config
+    mongoAuth: {                                // Optional DB Authentication
+        username: 'test',
+        password: 'test',
+        options: {}                             // Optional Authentication Config
+    },
+});
+```
+
+#### MongoDB URL and Connection Options
+
+These options are passed during the connection of a new MongoClient.
+
+Reference: [MongoClient API Doc](http://mongodb.github.io/node-mongodb-native/2.2/api/MongoClient.html)
+
+#### MongoDB Authentication
+
+These options are used to authenticate CastMyData with the MongoDB Instance.
+
+Reference: [db.authenticate API Doc](http://mongodb.github.io/node-mongodb-native/2.2/api/Db.html#authenticate)
+
+## Testing
+
+```bash
+> npm test
+
+  CastMyData MongoDB Tests
+    ✓ should deny api request without auth token
+    ✓ should be able to create a new record (99ms)
+    ✓ should be able list all records
+    ✓ should be able to get a record by id
+    ✓ should be able to update a record by id
+    ✓ should be able to delete a record by id
+    ✓ should be able to clear db
+    ✓ should be able to broadcast data
+    ✓ should be able to subscribe
+    ✓ should be able to create a record (67ms)
+    ✓ should be able to find a record by id
+    ✓ should be able to query an endpoint
+    ✓ should be able to update query models when a new record is created
+    ✓ should be able to update a record by id
+    ✓ should be able to delete a record by id
+    ✓ should be able to clear db
+    ✓ should be able to unsubscribe
+    ✓ should be able to close connection
+
+  18 passing (1s)
+```
+
+## License
+
+The MIT License (MIT)
+Copyright (c) 2016 Zulfa Juniadi
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ test/test.js
@@ -0,0 +1,221 @@
+var should = require('should');
+var request = require('request');
+var fs = require('fs');
+var path = require('path');
+var rmdir = require('rmdir-sync');
+var url = 'http://localhost:8080';
+var apiUrl = url + '/db/mochatest/';
+var dotenv = require('dotenv').config({
+    path: path.join(__dirname, '.castmydata.env')
+});
+var endpoint;
+
+// cleanup localstorage
+var localStoragePath = path.join(__dirname, '..', 'scratch');
+rmdir(localStoragePath);
+fs.mkdirSync(localStoragePath);
+
+// require castmydata client
+var client = require('castmydata-jsclient');
+
+describe('CastMyData MongoDB Tests', function() {
+
+    var castmydata = require('castmydata-server');
+
+    this.timeout(5000);
+
+    var api = request.defaults({
+        headers: {
+            'content-type': 'application/json',
+            'Authorization': 'Bearer ' + process.env.API_TOKEN
+        }
+    })
+
+    before(function(done) {
+        endpoint = new client.Endpoint(url, 'mochatest');
+        castmydata.start({
+            db: require('../index.js'),
+            mongoURL: 'mongodb://localhost:27017/test',
+            mongoConnOptions: {}
+        }, done);
+    });
+
+    after(function(done) {
+        this.timeout = 10000;
+        endpoint.close();
+        castmydata.stop(done);
+    });
+
+    it('should deny api request without auth token', function(done) {
+        request(apiUrl, function(error, response, body) {
+            response.statusCode.should.eql(403);
+            body.should.eql('Forbidden');
+            done();
+        });
+    });
+
+    var id;
+    it('should be able to create a new record', function(done) {
+        api.post(apiUrl, function(error, response, body) {
+            var data = JSON.parse(body);
+            response.statusCode.should.eql(201);
+            data.should.have.propertyByPath('response', 'title').eql('Buy Milk');
+            data.should.have.propertyByPath('response', 'meta', 'createdAt').should.be.ok();
+            data.response.id.should.be.ok();
+            id = data.response.id;
+            done();
+        }).form({
+            title: 'Buy Milk'
+        });
+    });
+
+    it('should be able list all records', function(done) {
+        api.get(apiUrl, function(err, response, body) {
+            var data = JSON.parse(body);
+            response.statusCode.should.eql(200);
+            data.should.have.property('response');
+            data.response.length.should.be.ok();
+            done();
+        });
+    });
+
+    it('should be able to get a record by id', function(done) {
+        api.get(apiUrl + id, function(err, response, body) {
+            var data = JSON.parse(body);
+            response.statusCode.should.eql(200);
+            data.should.have.propertyByPath('response', 'title').eql('Buy Milk');
+            data.should.have.propertyByPath('response', 'meta', 'createdAt').should.be.ok();
+            data.response.id.should.be.eql(id);
+            done();
+        });
+    });
+
+    it('should be able to update a record by id', function(done) {
+        api.put(apiUrl + id, function(err, response, body) {
+            var data = JSON.parse(body);
+            response.statusCode.should.eql(200);
+            data.should.have.propertyByPath('response', 'title').eql('Buy Eggs');
+            data.should.have.propertyByPath('response', 'meta', 'updatedAt').should.be.ok();
+            data.response.id.should.be.eql(id);
+            done();
+        }).form({
+            title: 'Buy Eggs'
+        });
+    });
+
+    it('should be able to delete a record by id', function(done) {
+        api.delete(apiUrl + id, function(err, response, body) {
+            var data = JSON.parse(body);
+            response.statusCode.should.eql(200);
+            data.response.id.should.be.eql(id);
+            data.should.have.propertyByPath('response', 'meta', 'deletedAt').should.be.ok();
+            done();
+        });
+    });
+
+    it('should be able to clear db', function(done) {
+        api.get(apiUrl + 'clear', function(err, response) {
+            response.statusCode.should.eql(200);
+            done();
+        });
+    });
+
+    it('should be able to broadcast data', function(done) {
+        api.post(apiUrl + 'broadcast', function(err, response, body) {
+            response.statusCode.should.eql(200);
+            done();
+        }).form({
+            channel: 'some-channel',
+            payload: 'Hello World',
+        });
+    });
+
+    it('should be able to subscribe', function(done) {
+        this.slow(2000);
+        endpoint.subscribe(function(){
+            done();
+        }).should.be.ok();
+    });
+
+    var id;
+    it('should be able to create a record', function(done) {
+        endpoint.post({
+            title: 'Buy Eggs'
+        }, function(model){
+            model.should.have.propertyByPath('title').eql('Buy Eggs');
+            model.should.have.propertyByPath('meta', 'createdAt').should.be.ok();
+            model.id.should.be.ok();
+            id = model.id;
+            done();
+        });
+    });
+
+    it('should be able to find a record by id', function(done) {
+        var model = endpoint.find(id);
+        model.should.be.ok();
+        model.id.should.eql(id);
+        done();
+    });
+
+    var query;
+    it('should be able to query an endpoint', function(done) {
+        query = endpoint.where(function(model) {
+            return model.title == 'Buy Eggs';
+        });
+        query.models.length.should.be.eql(1);
+        query.models[0].id.should.eql(id);
+        done();
+    });
+
+    var id2;
+    it('should be able to update query models when a new record is created', function(done) {
+        endpoint.post({
+            title: 'Buy Eggs'
+        }, function(model){
+            query.models.length.should.be.eql(2);
+            id2 = model.id;
+            done();
+        });
+    });
+
+    it('should be able to update a record by id', function(done) {
+        endpoint.put(id, {
+            title: 'Buy Bread'
+        }, function(model){
+            model.should.have.property('title').eql('Buy Bread');
+            model.should.have.propertyByPath('meta', 'updatedAt').should.be.ok();
+            model.id.should.eql(id);
+            query.models.length.should.be.eql(1);
+            done();
+        }).should.be.ok();
+    });
+
+    it('should be able to delete a record by id', function(done) {
+        endpoint.delete(id2, function(model){
+            model.should.not.have.property('title');
+            model.should.have.propertyByPath('meta', 'deletedAt').should.be.ok();
+            model.id.should.eql(id2);
+            query.models.length.should.eql(0);
+            done();
+        }).should.be.ok();
+    });
+
+    it('should be able to clear db', function(done) {
+        endpoint.clear(function(){
+            endpoint.models.length.should.eql(0);
+            done();
+        }).should.be.ok();
+    });
+
+    it('should be able to unsubscribe', function(done) {
+        endpoint.unsubscribe(function(){
+            endpoint.models.length.should.eql(0);
+            done();
+        }).should.be.ok();
+    });
+
+    it('should be able to close connection', function(done) {
+        endpoint.close().should.be.ok();
+        done();
+    });
+});
